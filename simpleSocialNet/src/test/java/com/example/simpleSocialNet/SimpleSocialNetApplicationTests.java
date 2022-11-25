d347e7ed26abe17dbccfcf3d457ba3d9522ee6c8
YakovRadaev <117990039+YakovRadaev@users.noreply.github.com>
2022-11-25 17:21:49 +0300

    RestApi

---
+++ simpleSocialNet/src/test/java/com/example/simpleSocialNet/SimpleSocialNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleSocialNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSocialNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

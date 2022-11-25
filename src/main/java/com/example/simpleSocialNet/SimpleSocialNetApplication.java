0e98a95058c3b5f97382dab74fd7007884516d6c
YakovRadaev <yakov.radaev@yandex.ru>
2022-11-25 17:18:03 +0300

    RestApi

---
+++ src/main/java/com/example/simpleSocialNet/SimpleSocialNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleSocialNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSocialNetApplication.class, args);
+	}
+
+}
